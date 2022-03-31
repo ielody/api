b33362f702b319e4094700d529f4d58fdbedec94
Ingeborg Eldøy <ieldoy@gmail.com>
2022-03-31 15:46:44 +0200

    Applied corrected version

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,35 +1,33 @@
 module.exports = async function($) {
   $.page.title = 'Create incredible web apps'
 
-  return /* html */`
-    <form onsubmit="return false">
-      <label for="title">Title</label>
-      <input id="title" type="text" name="title">
-      <em class="error-title"></em>
-      <button onclick="handleSubmit(this)">Save</button>
-    </form>
-
-    <script>
-    // Set up the Waveorb client
-    var api = waveorb('https://waveorb.com/api')
-
-    // Define your submit function
-    function handleSubmit(btn) {
+  // Define your submit function
+  async function handleSubmit(btn) {
     // Using the Haka form serializer to gather the data
     var values = serialize(btn.form)
 
     // Send the data to the action
-    var result = await api({ action: 'project/create', values })
-    if (result.error) {
-    // Join all the errors and display under the right input
-        Object.keys(result.values).forEach(function(key) {
-        text(`.error-${key}`, result.values[key].join(', '))
-      })
-      } else {
-    // Redirect to project list
-        window.location = '/projects'
-      }
+    var result = await api({
+      action: 'project/create',
+      values
+    })
+    console.log(result)
+    if (!showErrors(result)) {
+      cookie('flash', 'Project created')
+      // Redirect to project list
+      window.location = '/'
     }
+  }
+
+  return /* html */`
+    <form onsubmit="return false">
+      <label for="title">Title</label>
+      <input id="title" type="text" name="title">
+      <em class="title-errors"></em>
+      <button onclick="handleSubmit(this)">Save</button>
+    </form>
+    <script>
+      ${handleSubmit}
     </script>
   `
 }
